124da085ca13d27f8f7a4264e915b33efa535ca6
BiancaJvRensburg <34648959+BiancaJvRensburg@users.noreply.github.com>
2020-04-15 13:20:44 +0200

    Plane orientations updated

--- multiView/viewerfibula.cpp
+++ multiView/viewerfibula.cpp
@@ -17,7 +17,8 @@ void ViewerFibula::bendPolylineNormals(const std::vector<Vec>& normals){
     for(unsigned int i=0; i<normals.size(); i++){
         Vec binormal(0,1,0);
         Vec z = cross(normals[i], binormal);
-        ghostPlanes[i]->setFrameFromBasis(normals[i], binormal, z);
+        if(i%2==0) ghostPlanes[i]->setFrameFromBasis(normals[i], binormal, z);
+        else ghostPlanes[i]->setFrameFromBasis(normals[i], -binormal, -z);
         ghostPlanes[i]->setPosition(poly.getPoint((i+2)/2));
     }
 
@@ -35,6 +36,7 @@ void ViewerFibula::initGhostPlanes(){
         Plane *p1 = new Plane(1., Movable::STATIC, pos, .5f, i+1);
         ghostPlanes.push_back(p1);
         ghostPlanes[i]->setPosition(poly.getPoint((i+2)/2));
-        ghostPlanes[i]->setFrameFromBasis(Vec(0,0,1), Vec(0,-1,0), Vec(1,0,0));
+        if(i%2==0) ghostPlanes[i]->setFrameFromBasis(Vec(0,0,1), Vec(0,-1,0), Vec(1,0,0));
+        else ghostPlanes[i]->setFrameFromBasis(Vec(0,0,1), Vec(0,1,0), Vec(-1,0,0));
     }
 }
